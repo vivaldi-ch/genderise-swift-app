0ae398096c309b4ceb05afb6d874492a0da7a28c
Vivaldi Ibelio Chandra <ibel@verso.digital>
2019-02-16 12:34:03 +0800

    Initial commit.

---
+++ genderise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  genderise
+//
+//  Created by Vivaldi Ibelio Chandra on 16/2/19.
+//  Copyright © 2019 Vivaldi Ibelio Chandra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ genderiseTests/genderiseTests.swift
@@ -0,0 +1,34 @@
+//
+//  genderiseTests.swift
+//  genderiseTests
+//
+//  Created by Vivaldi Ibelio Chandra on 16/2/19.
+//  Copyright © 2019 Vivaldi Ibelio Chandra. All rights reserved.
+//
+
+import XCTest
+@testable import genderise
+
+class genderiseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
